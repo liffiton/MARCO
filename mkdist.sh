4a07434f4cdfd6c2a4178becffcfd872939c537c
Mark Liffiton <liffiton@gmail.com>
2013-05-19 22:04:52 -0400

    Real close to a nice distribution now...

--- mkdist.sh
+++ mkdist.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# gather whitelist of files to include
-marco_files="*.py README"
+# hack to get limited version of pyminisolvers
+cd pyminisolvers ; git checkout minisat_only ; cd ..
+
+# gather "whitelist" of files to include
+marco_files="*.py muser2-static README"
 test_files="tests/*.cnf tests/*.smt2 tests/*.gz tests/*.py tests/out/*"
 minisolvers_files=`find pyminisolvers/ -name "*.cc" -or -name "*.cpp" -or -name "*.h" -or -name "Makefile" -or -name "makefile" -or -name "*.py"`
 
@@ -29,3 +32,5 @@ tar czvhf $dir.tar.gz $dir
 # cleanup
 rm -r $dir
 
+# de-hack
+cd pyminisolvers ; git checkout master ; cd ..
