f130e66de26e3fadc47ec9aa37b3b789eaf12df8
Mark Liffiton <liffiton@gmail.com>
2013-05-28 21:41:03 -0500

    Assume *all* of the aux. vars for setting the cardinality constraint bound.

--- MinicardMapSolver.py
+++ MinicardMapSolver.py
@@ -24,8 +24,7 @@ class MinicardMapSolver:
         self.solver.add_atmost([-(x+1) for x in range(self.n * 2)], self.n)
 
     def solve_with_bound(self, k):
-        #return self.solver.solve([-(x+1+self.n) for x in range(k)])
-        return self.solver.solve([-(x+1+self.n) for x in range(k)])
+        return self.solver.solve([-(x+1+self.n) for x in range(k)] + [(x+1)+self.n+k for x in range(self.n-k)])
 
     def has_seed(self):
         '''
