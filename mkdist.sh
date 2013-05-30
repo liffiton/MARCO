f801d5f4eff5eb0bf70853c76b7bc70fdbfc88d7
Mark Liffiton <liffiton@gmail.com>
2013-05-30 12:34:04 -0500

    Update mkdist.sh; include minicard.

--- mkdist.sh
+++ mkdist.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 # hack to get limited version of pyminisolvers
-cd pyminisolvers ; git checkout minisat_only ; cd ..
+#cd pyminisolvers ; git checkout minisat_only ; cd ..
 
 # gather "whitelist" of files to include
 marco_files="*.py muser2-static README VERSION"
 test_files="tests/*.cnf tests/*.smt2 tests/*.gz tests/*.py tests/out/*"
-minisolvers_files=`find pyminisolvers/ -path "pyminisolvers/minicard" -prune -o \( -name "*.cc" -or -name "*.cpp" -or -name "*.h" -or -name "Makefile" -or -name "makefile" -or -name "*.py" \) -print`
+minisolvers_files=`find pyminisolvers/ \( -name "*.cc" -or -name "*.cpp" -or -name "*.h" -or -name "Makefile" -or -name "makefile" -or -name "*.py" \) -print`
+
+if [ "$1" = "list" ] ; then
+    echo "Selected files:"
+    for f in $marco_files $test_files $minisolvers_files ; do
+        echo $f
+    done
+    exit 0
+fi
 
 # setup temp named dir
 version=`cat VERSION`
@@ -33,4 +41,4 @@ tar czvhf $dir.tar.gz $dir
 rm -r $dir
 
 # de-hack
-cd pyminisolvers ; git checkout master ; cd ..
+#cd pyminisolvers ; git checkout master ; cd ..
