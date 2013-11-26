7ce5f22abe899ef1a79455fdb219819e6e9f6371
Mark Liffiton <liffiton@gmail.com>
2013-11-26 15:33:20 -0600

    Minor tweaks to timing code, added current_time().

--- utils.py
+++ utils.py
@@ -1,19 +1,22 @@
 """Utility class(es) for marco_py"""
-
-# time.clock() is not portable - behaves differently per OS
-# TODO: Consider using time.process_time() (only in 3.3, though)
-import time
-#import os
 from collections import Counter, defaultdict
 
-#get_time = lambda: sum(os.times()[:4])  # combined user/sys time for this process and its children
-get_time = time.time   # wall-time
-#get_time = time.clock   # user-time
+# Three options for measuring time: choose one.
+# TODO: Consider using time.process_time() (only in 3.3, though)
+
+import time
+_get_time = time.time   # wall-time
+
+# time.clock() is not portable - behaves differently per OS
+#_get_time = time.clock   # user-time
+
+#import os
+#_get_time = lambda: sum(os.times()[:4])  # combined user/sys time for this process and its children
 
 
 class Statistics:
     def __init__(self):
-        self._start = get_time()
+        self._start = _get_time()
         self._times = Counter()
         self._counts = Counter()
         self._stats = defaultdict(list)
@@ -43,18 +46,21 @@ class Statistics:
 
     def start_time(self):
         self._counts[self._category] += 1
-        self._curr = get_time()
+        self._curr = _get_time()
 
     def end_time(self):
-        self._times[self._category] += get_time() - self._curr
+        self._times[self._category] += _get_time() - self._curr
         self._category = None
 
+    def current_time(self):
+        return _get_time() - self._start
+
     def get_times(self):
-        self._times['total'] = get_time() - self._start
+        self._times['total'] = self.current_time()
         if self._category:
             # If we're in a category currently,
             # give it the time up to this point.
-            self._times[self._stats._category] += get_time() - self._curr
+            self._times[self._stats._category] += _get_time() - self._curr
 
         return self._times
 
