e3e749213307ed36423194eadc09128e2ff106b0
Mark Liffiton <liffiton@gmail.com>
2015-05-28 23:04:49 -0500

    Bugfix and other improvements in Statistics class.

--- utils.py
+++ utils.py
@@ -14,25 +14,50 @@ _get_time = time.time   # wall-time
 #_get_time = lambda: sum(os.times()[:4])  # combined user/sys time for this process and its children
 
 
-class Statistics:
+class Statistics(object):
+    """
+    >>> import time   # for time.sleep() in below examples
+    >>> s = Statistics()
+
+    The Statistics object provides a simple method for timing blocks of
+    code with a context manager.  Timed blocks may be nested.
+    >>> with s.time("outer"):
+    ...     time.sleep(0.1)
+    ...     with s.time("inner"):
+    ...         time.sleep(0.05)
+    >>> ["%s: %0.2f" % (key, value) for key, value in s.get_times().items()]
+    ['total: 0.15', 'outer: 0.15', 'inner: 0.05']
+
+    Times are accumulated across all calls to time() that share a 'category',
+    and the count for each category (how many times it was measured) is
+    available for simple averaging or other analysis.
+    >>> with s.time("outer"):
+    ...     time.sleep(0.02)
+    >>> ["%s: %0.2f" % (key, value) for key, value in s.get_times().items()]
+    ['total: 0.17', 'outer: 0.17', 'inner: 0.05']
+    >>> s.get_counts()
+    Counter({'outer': 2, 'inner': 1})
+
+    The object also provides a method for collecting arbitrary statistics.
+    Every value added for a given string is appended to a list.
+    >>> s.add_stat('statA', 5)
+    >>> s.add_stat('statB', 123)
+    >>> s.add_stat('statA', 8)
+    >>> dict(s.get_stats())
+    {'statB': [123], 'statA': [5, 8]}
+    """
     def __init__(self):
         self._start = _get_time()
         self._times = Counter()
         self._counts = Counter()
         self._stats = defaultdict(list)
-        self._timer_categories = set()  # track which timers are currently running
+        self._active_timers = {}   # dict: key=category, value=start time
 
-    # Usage:
-    #  s = Statistics()
-    #  with s.time("one")
-    #    # do first thing
-    #  with s.time("two")
-    #    # do second thing
     def time(self, category):
-        return self.Timer(self, category)
+        return self.TimerContext(self, category)
 
     # Context manager class for time() method
-    class Timer:
+    class TimerContext(object):
         def __init__(self, stats, category):
             self._stats = stats
             self._category = category
@@ -45,24 +70,29 @@ class Statistics:
             return False  # doesn't handle any exceptions itself
 
     def start_time(self, category):
+        assert category not in self._active_timers
         self._counts[category] += 1
-        self._timer_categories.add(category)
-        self._curr = _get_time()
+        self._active_timers[category] = _get_time()
 
-    def end_time(self, category, remove=True):
-        self._times[category] += _get_time() - self._curr
-        if remove:
-            self._timer_categories.remove(category)
+    def end_time(self, category):
+        self.update_time(category)
+        del self._active_timers[category]
 
-    def current_time(self):
+    def update_time(self, category):
+        now = _get_time()
+        self._times[category] += now - self._active_timers[category]
+        # reset the "start time" as previous time is now counted
+        self._active_timers[category] = now
+
+    def total_time(self):
         return _get_time() - self._start
 
     def get_times(self):
-        self._times['total'] = self.current_time()
-        for category in self._timer_categories:
+        self._times['total'] = self.total_time()
+        for category in self._active_timers:
             # If any timers are currently running,
             # give them the time up to this point.
-            self.end_time(category, remove=False)
+            self.update_time(category)
 
         return self._times
 
